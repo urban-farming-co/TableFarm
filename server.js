89476ce9e2851b890d566548c1c9d4f495cb2ff0
Karen McCulloch <k.a.z.mcc95@gmail.com>
2016-08-24 14:42:40 +0100

    set up the values of the database to show on the holding page [test]

--- server.js
+++ server.js
@@ -2,36 +2,42 @@ console.error('Starting');
 var fs     = require('fs');
 var http   = require('http');
 var sqlite = require('sqlite3');
-var db     = new sqlite.Database("db.sqlite");
+var db     = new sqlite.Database("holdingDash.sqlite");
 
 var tableCheck = "SELECT name FROM sqlite_master WHERE type='table' AND name='LOREM';";
 db.get(tableCheck, function(err, exists) {
-      if (exists.length == 0) {
-              db.serialize(function() {
-                        db.run("CREATE TABLE LOREM (info TEXT)");
-                              
-                              var stmt = db.prepare("INSERT INTO LOREM VALUES (?)");
-                                    for (var i=0; i<10; i++) {
-                                                stmt.run("Ipsum " +i);
-                                                      }
-                                          stmt.finalize();
-                                              });
-                }
+    if (exists.length == 0) {
+        db.serialize(function() {
+            db.run("CREATE TABLE tbl1 (id, image, soilMoisture, relHumidity, Temp )");
+            db.run("INSERT INTO tbl1 VALUES (1, , 100, 100, 100)");
+        });
+    }
 });
 
 
 
-  
+
 
 http.createServer(function(req, res) {
-      var layout  = fs.readFileSync('index.html', 'utf8');
-        var content = '';
-          db.each("SELECT rowid AS id, info FROM lorem", function(err, row) {
-                  content += row.id + ": " + row.info + "<br/>";
-                    }, function() {
-                            res.end(layout.replace("{{content}}", content));
-                              });
+    var layout  = fs.readFileSync('index.html', 'utf8');
+    var content = '';
+    db.each("SELECT * FROM tbl1 WHERE id=(SELECT MAX(id) FROM tbl1 ", function(err, row) {
+        content +="
+            <th>Image</th>
+            <td>row.image</td>
+            </tr><tr>
+            <th>Soil Moisture</th>
+            <td>row.soilMoisture</td>
+            </tr><tr>
+            <th>Relative Humidity</th>
+            <td>row.relHumidity</td>
+            </tr><tr>
+            <th>Temperature</th>
+            <td>row.Temp</td>";
+    }, function() {
+        res.end(layout.replace("{{content}}", content));
+    });
 }).listen(3000, function() {
-      console.log('Listening');
+    console.log('Listening');
 });
 
