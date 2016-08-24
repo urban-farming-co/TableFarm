a0d9f60c9ab95392840395a6e472f35c226a6bc4
kazzacarrot <kazzacarrot@hotmail.co.uk>
2016-08-24 14:10:35 -0400

    add stuff

--- server.js
+++ server.js
@@ -4,8 +4,9 @@ var http   = require('http');
 var sqlite = require('sqlite3');
 var db     = new sqlite.Database("holdingDash.sqlite");
 
-var tableCheck = "SELECT name FROM sqlite_master WHERE type='table' AND name='LOREM';";
+var tableCheck = "SELECT name FROM sqlite_master WHERE type='table' AND name='tbl1';";
 db.get(tableCheck, function(err, exists) {
+    if (err) {console.error(error)};
     if (exists.length == 0) {
         db.serialize(function() {
             db.run("CREATE TABLE tbl1 (id, image, soilMoisture, relHumidity, Temp )");
@@ -20,24 +21,15 @@ db.get(tableCheck, function(err, exists) {
 
 http.createServer(function(req, res) {
     var layout  = fs.readFileSync('index.html', 'utf8');
+    console.log(layout);
     var content = '';
-    db.each("SELECT * FROM tbl1 WHERE id=(SELECT MAX(id) FROM tbl1 ", function(err, row) {
-        content +="
-            <th>Image</th>
-            <td>row.image</td>
-            </tr><tr>
-            <th>Soil Moisture</th>
-            <td>row.soilMoisture</td>
-            </tr><tr>
-            <th>Relative Humidity</th>
-            <td>row.relHumidity</td>
-            </tr><tr>
-            <th>Temperature</th>
-            <td>row.Temp</td>";
+    db.each("SELECT * FROM tbl1 WHERE id=(SELECT MAX(id) FROM tbl1) ", function(err, row) {
+    if (err) {console.error(err)};
+        content +="<th>Image</th> <td>row.image</td> </tr><tr> <th>Soil Moisture</th> <td>row.soilMoisture</td> </tr><tr> <th>Relative Humidity</th> <td>row.relHumidity</td> </tr><tr> <th>Temperature</th> <td>row.Temp</td>";
     }, function() {
         res.end(layout.replace("{{content}}", content));
     });
-}).listen(3000, function() {
+}).listen(80, function() {
     console.log('Listening');
 });
 
