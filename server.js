b5fde4fc81d558f1f316f7cad919fd9cca3cec39
Karen McCulloch <k.a.z.mcc95@gmail.com>
2016-08-26 20:39:23 +0100

    start using express.

--- server.js
+++ server.js
@@ -3,33 +3,33 @@ var fs     = require('fs');
 var http   = require('http');
 var sqlite = require('sqlite3');
 var db     = new sqlite.Database("holdingDash.sqlite");
-const express = require('express')
-const app = express()
-const port = 4000
+var port = 4000;
+var path = require('path');
+var express = require('express');
+var exphbs = require('express-handlebars');
+var app = express();
+app.use(express.static(path.join(__dirname, 'views')))
 
-app.get('/', (request, response) => {
-    respons.send('Hello from Express!')
-})
-
-
-
-var tableCheck = "SELECT name FROM sqlite_master WHERE type='table' AND name='tbl1';";
-db.get(tableCheck, function(err, exists) {
-    if (err) {console.error(error)};
-    if (exists.length == 0) {
+var tableCheck = "SELECT 1 FROM sqlite_master WHERE type='table' AND name='tbl1';";
+db.get(tableCheck, function(err, row) {
+    if (err) {console.error(err)};
+    if (row == undefined ) {
         db.serialize(function() {
-            db.run("CREATE TABLE tbl1 (id, image, soilMoisture, relHumidity, Temp )");
-            db.run("INSERT INTO tbl1 VALUES (1, , 100, 100, 100)");
+            db.run("CREATE TABLE tbl1 (id, image, soilMoisture, relHumidity, Temp )", function(err){console.error(err)});
+            db.run("INSERT INTO tbl1 VALUES (1, ,100 ,100 , 100)", function(err){console.error(err)});
         });
     }
 });
 
 
+app.engine('.hbs', exphbs({
+    defaultLayout: 'layout',
+    extname:'.hbs',
+    layoutsDir:path.join(__dirname, 'public')
+}))
 
-
-
-http.createServer(function(req, res) {
-    var layout  = fs.readFileSync('index.html', 'utf8');
+const requestHandler = (req, res) => {
+    var layout  = fs.readFileSync('Functions/HoldingPage/index.hbs', 'utf8');
     console.log(layout);
     var content = '';
     db.each("SELECT * FROM tbl1 WHERE id=(SELECT MAX(id) FROM tbl1) ", function(err, row) {
@@ -38,7 +38,15 @@ http.createServer(function(req, res) {
     }, function() {
         res.end(layout.replace("{{content}}", content));
     });
-}).listen(4000, function() {
-    console.log('Listening');
+}
+
+app.set('view engine', '.hbs')
+app.set('views', path.join(__dirname, ''))
+const server = http.createserver().listen(port, requestHandler) 
+server.listen(port, (err) => {
+    if (err) {
+        return console.log('something bad happened')
+    }
+    server.log('Listening');
 });
 
