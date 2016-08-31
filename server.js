10cc5376f6c245374497ef7ac693f4cfc8badc30
Karen McCulloch <k.a.z.mcc95@gmail.com>
2016-08-31 21:23:59 +0100

    make it more stylish.

--- server.js
+++ server.js
@@ -38,6 +38,13 @@ app.use((request, response, next) => {
         next()
 })
 
+app.use(function(req, res, next) {
+    if(req.url.substr(-1) !== '/')
+        res.redirect(301, req.url + "/");
+    else
+        next();
+});
+
 function getNextId(callback) {
     db.each("SELECT * FROM tbl1 WHERE id=(SELECT MAX(id) FROM tbl1) ", function(err, row) {
         if (err) {
@@ -54,7 +61,7 @@ function getHome(request, response, callback)  {
             return callback(err)
         }
 
-        content +="<th></th> <td><img width=50px height=50px  src=" + row.image+ " /></td> </tr> <tr><th>Date/Time:</th><td>"+row.time+"</td><tr><th>Plant name:</th><td>"+row.plantName+"</td></tr><tr><th>Lighting lux level:</th><td>"+row.lightLuxLevel+"<tr> <th>Soil Moisture: </th> <td>"+row.soilMoisture+"%</td> </tr><tr> <th>Relative Humidity:</th> <td>"+row.relHumidity+"%</td> </tr><tr> <th>Ambient temperature:</th> <td>"+row.temperature+" C</td>";
+        content +="<th></th> <td><img width=50px height=50px  src=" + row.image+ " /></td> </tr> <tr><th>Date/Time:</th><td>"+row.time+"</td><tr><th>Plant name:</th><td>"+row.plantName+"</td></tr><tr><th>Lighting lux level:</th><td>"+row.lightLuxLevel+" lux <tr> <th>Soil Moisture: </th> <td>"+row.soilMoisture+"%</td> </tr><tr> <th>Relative Humidity:</th> <td>"+row.relHumidity+"%</td> </tr><tr> <th>Ambient temperature:</th> <td>"+row.temperature+" C</td>";
         callback(null, content)
     })
 }
@@ -88,8 +95,8 @@ app.post('/urbanfarming/data', function(req, res){
             res.end(util.inspect({fields:fields, files:files}));
             var fileextention = files.image[0].originalFilename.split('.').pop();
             var target = "./public/images/"+id+"." +fileextention
-            fs.rename(files.image[0].path, target) 
-            console.log(fields.soilMoisture[0]);
+                fs.rename(files.image[0].path, target) 
+                console.log(fields.soilMoisture[0]);
             console.log(fields.relHumidity[0]);
             console.log(fields.temp[0]);
             console.log(files.image[0].name);
