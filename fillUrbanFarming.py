0615f2f7295fc4d69bc54a8f57cff0c9107c1496
Karen McCulloch <k.a.z.mcc95@gmail.com>
2016-08-31 18:02:30 +0100

    make jasons changes

---
+++ fillUrbanFarming.py
@@ -0,0 +1,21 @@
+import pymysql
+
+connection = pymysql.connect(host='localhost',
+                             user='root',
+                             password='',
+                             db='db',
+                             charset='utf8mb4',
+                             cursorclass=pymysql.cursors.DictCursor)
+
+try:
+    with connection.cursor() as cursor:
+        sql = "INSERT INTO `plant_project` VALUES (%s, %s, %s, %s)"
+        cursor.execute(sql, ('1','Jain', 'blah', '2016-08-30 11:54:39'))
+    connection.commit()
+    with connection.cursor() as cursor:
+        sql = "SELECT `uid`, `user_name` FROM `plant_projects` WHERE `uid`=%s"
+        cursor.execute(sql, ('1'))
+        result = cursor.fetchone()
+        print(result)
+finally:
+    connection.close()
--- server.js
+++ server.js
@@ -4,7 +4,8 @@ var path    = require ('path');
 var sqlite  = require('sqlite3');
 var multiparty = require('multiparty')
 var db      = new sqlite.Database("holdingDash.sqlite");
-var port    = 4000;
+var port    = (process.env.VCAP_APP_PORT || 4000);
+var host    = (process.env.VCAP_APP_HOST || 'localhost');
 var express = require('express');
 var exphbs  = require('express-handlebars');
 var bodyParser = require('body-parser');
@@ -19,7 +20,7 @@ db.get(tableCheck, function(err, row) {
     if (err) {console.error(err)};
     if (row == undefined ) {
         db.serialize(function() {
-            db.run("CREATE TABLE tbl1 (id INT PRIMARY KEY, image VARCHAR(200), soilMoisture DOUBLE, relHumidity DOUBLE, temperature DOUBLE, time TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL )", function(err){if (err) {console.error(err)}});
+            db.run("CREATE TABLE tbl1 (id INT PRIMARY KEY, image VARCHAR(200), soilMoisture DOUBLE, relHumidity DOUBLE, temperature DOUBLE, time TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL, plantName VARCHAR(50), lightLuxLevel DOUBLE )", function(err){if (err) {console.error(err)}});
             //db.run("INSERT INTO tbl1 (id, soilMoisture, relHumidity, temperature) VALUES (1,100 ,100 ,100)", function(err){if (err) {console.error(err)}});
         });
     }
@@ -53,7 +54,7 @@ function getHome(request, response, callback)  {
             return callback(err)
         }
 
-        content +="<th>Image</th> <td><img width=50px height=50px  src=" + row.image+ " /></td> </tr><tr> <th>Soil Moisture: </th> <td>"+row.soilMoisture+"%</td> </tr><tr> <th>Relative Humidity:</th> <td>"+row.relHumidity+"%</td> </tr><tr> <th>Temperature:</th> <td>"+row.temperature+" C</td><tr><th>Date/Time:</th><td>"+row.time+"</td>";
+        content +="<th></th> <td><img width=50px height=50px  src=" + row.image+ " /></td> </tr> <tr><th>Date/Time:</th><td>"+row.time+"</td><tr><th>Plant name:</th><td>"+row.plantName+"</td></tr><tr><th>Lighting lux level:</th><td>"+row.lightLuxLevel+"<tr> <th>Soil Moisture: </th> <td>"+row.soilMoisture+"%</td> </tr><tr> <th>Relative Humidity:</th> <td>"+row.relHumidity+"%</td> </tr><tr> <th>Ambient temperature:</th> <td>"+row.temperature+" C</td>";
         callback(null, content)
     })
 }
@@ -92,8 +93,11 @@ app.post('/urbanfarming/data', function(req, res){
             console.log(fields.relHumidity[0]);
             console.log(fields.temp[0]);
             console.log(files.image[0].name);
+            console.log(fields.plantName[0]);
+            console.log(fields.lightLuxLevel[0]);
             console.log(`INSERT INTO tbl1 (id, soilMoisture,relHumidity, temperature, image  ) VALUES (${id},${fields.soilMoisture[0]}, ${fields.relHumidity[0]}, ${fields.temp[0]}, '${files.image[0].path}')`)
-                db.run(`INSERT INTO tbl1 (id, soilMoisture,relHumidity, temperature, image  ) VALUES (${id},${fields.soilMoisture[0]}, ${fields.relHumidity[0]}, ${fields.temp[0]},'${target}')`, function(err){if (err) {console.error("error on 93 "+ err)}});
+
+                db.run(`INSERT INTO tbl1 (id, soilMoisture,relHumidity, temperature, image, plantName, lightLuxLevel  ) VALUES (${id},${fields.soilMoisture[0]}, ${fields.relHumidity[0]}, ${fields.temp[0]},'${target}', fields.plantName[0], fields.lightLuxLevel[0] )`, function(err){if (err) {console.error("error on 93 "+ err)}});
 
         });
     })
