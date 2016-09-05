a45c049768f6bb0c07a401a1b8c79b8e045c41d7
Karen McCulloch <k.a.z.mcc95@gmail.com>
2016-09-05 11:00:57 +0100

    add table check to add raspberry pi server

--- Functions/addRaspberryPi/server.js
+++ Functions/addRaspberryPi/server.js
@@ -9,15 +9,24 @@ var express = require('express');
 var app     = express();
 
 
+var tableCheck = "SELECT 1 FROM sqlite_master WHERE type='table' AND name='piToUser';";
+db.get(tableCheck, function(err, row) {
+    if (err) {console.error(err)};
+    if (row == undefined ) {
+        db.serialize(function() {
+            db.run("CREATE TABLE piToUser (id INT PRIMARY KEY, image VARCHAR(200), soilMoisture DOUBLE, relHumidity DOUBLE, temperature DOUBLE, time TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL, plantName VARCHAR(50), lightLuxLevel DOUBLE )", function(err){if (err) {console.error(err)}});
+            //db.run("INSERT INTO tbl1 (id, soilMoisture, relHumidity, temperature) VALUES (1,100 ,100 ,100)", function(err){if (err) {console.error(err)}});
+        });
+    }
+});
 app.get('registerRaspberry', (request, response) => {
     response.write("raspberry pi registration area, no humans allowed.");
 });
 
 
 app.post("registerRaspberry", (request, response) => {
-    var form = new multiparty.Form();
-    form.parse(req, function(err, fields, files){
-
-        
+    Object.keys(request).forEach(function(name){ ;
+        console.log('got header named ' + name);
+    });
 
 });
