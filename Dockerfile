399169134dcccba51f249ebbd90a484654efa8e4
Karen McCulloch <k.a.z.mcc95@gmail.com>
2016-09-28 16:30:15 +0100

    make docker work!

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,32 @@
-FROM node:4-onbuild
+FROM node:argon
 MAINTAINER Karen McCulloch <k.a.z.mcc95@gmail.com>
 EXPOSE 4000   
+EXPOSE 5432   
+# copy the application folder inside the container
+RUN mkdir -p /usr/src/app
+COPY . /usr/src/app
+WORKDIR /usr/src/app
+# install system-wide deps for python
 RUN apt-get update 
+RUN apt-get -yqq install python python-pip
+RUN apt-get -yqq install pkg-config
+RUN apt-get -yqq install python-tk python-imaging
+RUN apt-get install -y libopenblas-dev liblapack-dev git-core build-essential gfortran libfreetype6-dev libjpeg-dev libhdf5-dev liblzo2-dev libbz2-dev
+RUN curl https://bootstrap.pypa.io/get-pip.py | python
+
+RUN apt-get -yqq install python-dev
+
+RUN pip install cython && \
+    pip install scipy  && \
+    pip install matplotlib && \
+    pip install six && \
+    pip install numpy && \
+    pip install scikit-image  
+
+RUN pip install image \ 
+    pillow 
+    
+
+# start up the node app.  
 RUN npm install
 CMD npm start
-
--- process_images/greenScore.py
+++ process_images/greenScore.py
@@ -4,7 +4,7 @@ try:
     import Image
 except ImportError:
     from PIL import Image
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 from skimage.io import imread
 from skimage import img_as_float
 import numpy as np
@@ -31,59 +31,80 @@ def tooLight(pixel, thres):
 
 
 def tooDark(pixel, thres2):
-    if pixel[0] >= thres2[0] and pixel[2] >= thres2[2] and pixel[1] >= thres2[1]:
+    if pixel[0] >= thres2[0]:
+        if pixel[2] >= thres2[2] and pixel[1] >= thres2[1]:
+            return True
+    return False
+
+
+def isRed(pixel):
+    # if the green part is greater than the blue and red part.
+    if pixel[0] >= (pixel[1] - 0.01) and pixel[1] >= (pixel[2] - 0.01):
         return True
     return False
 
 
+def isBlue(pixel):
+    # if the green part is greater than the blue and red part.
+    if pixel[2] >= (pixel[1] - 0.14) and pixel[2] >= (pixel[0] - 0.14):
+        return True
+    return False
+
+
+def isGreen(pixel):
+    # if the green part is greater than the blue and red part.
+    if pixel[1] >= (pixel[0] + 0.007) and pixel[1] >= (pixel[2] + 0.007):
+        return True
+    return False
+
 
 def filter(e, f, r):
     s = 0
-    filt2 = [0.9, 0.9, 0.9]
-    filt = [0.3, 0.3, 0.3]
+    # filt2 = [0.9, 0.9, 0.9]
+    # filt = [0.3, 0.3, 0.3]
     i = r.copy()
     for y in range(r.shape[0]):
         for x in range(r.shape[1]):
-            if (tooGray(r[y][x], i[y][x])):
-                #i[y][x][0] = 1          # red
-                #if not(tooDark(r[y][x], filt2)):
-                #    i[y][x][1] = 1      # green
-                #    if not(tooLight(r[y][x], filt)):
-                #        i[y][x][2] = 1  # blue
-                s += 0.5
+            # if not(tooGray(r[y][x], i[y][x])):
+            #     i[y][x][0] = 1          # red
+            #     if not(tooDark(r[y][x], filt2)):
+            #         i[y][x][1] = 1      # green
+            #         if not(tooLight(r[y][x], filt)):
+            #             i[y][x][2] = 1  # blue
+            if not(isBlue(r[y][x])):
+                if not(isRed(r[y][x])):
+                    if (isGreen(r[y][x])):
+                        i[y][x][0] = 1
+                        i[y][x][1] = 1
+                        s += 0.5
     score[e] = s
     return i, s
 
 
 def getScore(f):
     r = img_as_float(imread(f))
-    t = True
-    s = (8, 4)
-    fig1, (ax5, ax6) = plt.subplots(ncols=2, figsize=s, sharex=t, sharey=t)
+    # t = True
+    # s = (8, 4)
+    # fig1, (ax5, ax6) = plt.subplots(ncols=2, figsize=s, sharex=t, sharey=t)
 
     # if less; not green enough
-    rThreshold = 0.30
-    gThreshold = 0.50
-    bThreshold = 0.30
-
-    thres = np.array([rThreshold, gThreshold, bThreshold])
-    thres2 = np.array([0.4, 0.9, 0.3])
 
     m, s = filter(5, np.array([1, 1, 1]), r)
     im = Image.fromarray(np.uint8(m*255))
-    im.save("/home/karen/Repos/UrbanFarming/public/foo1.JPEG")
-    #ax5.imshow(r)
-    #ax6.imshow(m)
-    ##ax5.set_title("original", fontsize=20)
-    #ax6.set_title(score[5], fontsize=20)
+    im.save("public/foo1.JPEG")
+    # ax5.imshow(r)
+    # ax6.imshow(m)
+    # ax5.set_title("original", fontsize=20)
+    # ax6.set_title(score[5], fontsize=20)
 
-    #plt.show()
+    # plt.show()
     return score[5]
+
 
 if __name__ == '__main__':
     try:
         a = sys.argv[1]
     except:
-        a = "/home/karen/Repos/UrbanFarming/public/foo.jpg"
+        a = "public/foo.jpg"
     print(getScore(a))
     sys.stdout.flush()
