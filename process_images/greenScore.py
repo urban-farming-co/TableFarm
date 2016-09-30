c07aba99a901c6ff5a8a405fa79da98d3eba21e7
Karen McCulloch <k.a.z.mcc95@gmail.com>
2016-09-30 16:03:24 +0100

    refactor greenScore and add methods for width/height/compactness/colour

--- process_images/greenScore.py
+++ process_images/greenScore.py
@@ -4,37 +4,10 @@ try:
     import Image
 except ImportError:
     from PIL import Image
-# import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 from skimage.io import imread
 from skimage import img_as_float
 import numpy as np
-
-# image = [0, 0, 0] * r
-score = [0]*7
-# Construct some test data
-
-
-def tooGray(pixel, i):
-    if pixel[0] <= (pixel[2] + 0.21) and pixel[0] >= (pixel[2] - 0.21):
-        i[1] = 1
-        # if pixel[1] <= (pixel[2] + 0.02) and pixel[1] >= (pixel[2] - 0.32):
-        if pixel[1] <= (pixel[2] + 0.12) and pixel[1] >= (pixel[2] - 0.32):
-            i[0] = 1
-            return True
-    return False
-
-
-def tooLight(pixel, thres):
-    if pixel[0] < thres[0] and pixel[2] < thres[2] and pixel[1] < thres[1]:
-        return True
-    return False
-
-
-def tooDark(pixel, thres2):
-    if pixel[0] >= thres2[0]:
-        if pixel[2] >= thres2[2] and pixel[1] >= thres2[1]:
-            return True
-    return False
 
 
 def isRed(pixel):
@@ -58,53 +31,134 @@ def isGreen(pixel):
     return False
 
 
-def filter(e, f, r):
-    s = 0
-    # filt2 = [0.9, 0.9, 0.9]
-    # filt = [0.3, 0.3, 0.3]
+def findThePlant(r):
+    p = []
     i = r.copy()
     for y in range(r.shape[0]):
         for x in range(r.shape[1]):
-            # if not(tooGray(r[y][x], i[y][x])):
-            #     i[y][x][0] = 1          # red
-            #     if not(tooDark(r[y][x], filt2)):
-            #         i[y][x][1] = 1      # green
-            #         if not(tooLight(r[y][x], filt)):
-            #             i[y][x][2] = 1  # blue
+            # Find the plant within the image.
             if not(isBlue(r[y][x])):
                 if not(isRed(r[y][x])):
                     if (isGreen(r[y][x])):
-                        i[y][x][0] = 1
-                        i[y][x][1] = 1
-                        s += 0.5
-    score[e] = s
-    return i, s
+                        # add (y, x) to the list
+                        i[y][x][0] = i[y][x][1] = 1
+                        p.append((y, x))
+    return i, p
 
 
-def getScore(f):
+def readFile(f):
     r = img_as_float(imread(f))
-    # t = True
-    # s = (8, 4)
-    # fig1, (ax5, ax6) = plt.subplots(ncols=2, figsize=s, sharex=t, sharey=t)
+    return r
 
-    # if less; not green enough
 
-    m, s = filter(5, np.array([1, 1, 1]), r)
+def savePlantImage(m):
     im = Image.fromarray(np.uint8(m*255))
     im.save("public/foo1.JPEG")
-    # ax5.imshow(r)
-    # ax6.imshow(m)
-    # ax5.set_title("original", fontsize=20)
-    # ax6.set_title(score[5], fontsize=20)
+    return
 
-    # plt.show()
-    return score[5]
+
+def displayProcesses(s, r, p):
+    t = True
+    size = (8, 4)
+    fig1, (ax5, ax6) = plt.subplots(ncols=2, figsize=size, sharex=t, sharey=t)
+
+    ax5.imshow(r)
+    ax6.imshow(p)
+    ax5.set_title("original", fontsize=20)
+    ax6.set_title(s, fontsize=20)
+
+    plt.show()
+    return
+
+
+def getScore(p):
+    return 0.5 * len(p)
+
+
+def getMax(column, points):
+    """
+    Given the desired column - x or y, and the list of points, find the maximum
+    x or y of the list of points.
+    """
+    m = points[0][column]
+    for p in points:
+        if p[column] > m:
+            m = p[column]
+    return m
+
+
+def getMin(column, points):
+    """
+    Given the desired column - x or y, and the list of points, find the minimum
+    x or y of the list of points.
+    """
+    m = points[0][column]
+    for p in points:
+        if p[column] < m:
+            m = p[column]
+    return m
+
+
+def getWidth(p):
+    maxX = getMax(0, p)
+    minX = getMin(0, p)
+    return maxX - minX
+
+
+def getHeight(p):
+    maxY = getMax(1, p)
+    minY = getMin(1, p)
+    return maxY - minY
+
+
+def getAverageColour(column, points, image):
+    x = points[0][0]
+    y = points[0][1]
+    total = 0
+    for p in points:
+        x = p[0]
+        y = p[1]
+        total += image[x][y][column]
+    return total/len(points)
+
+
+def getAveragePlantColour(p, r):
+    averageR = getAverageColour(0, p, r) * 255
+    averageG = getAverageColour(1, p, r) * 255
+    averageB = getAverageColour(2, p, r) * 255
+    return (averageR, averageG, averageB)
+
+
+def getCompactness(width, height):
+    # is it as high as it is tall?
+    if (height == width):
+        return 0
+    elif (height > width):
+        return 1
+    else:
+        return -1
 
 
 if __name__ == '__main__':
     try:
         a = sys.argv[1]
     except:
-        a = "public/foo.jpg"
-    print(getScore(a))
+        a = "../public/foo.jpg"
+    picture = readFile(a)
+    processedImage, plantPoints = findThePlant(picture)
+
+    Score = getScore(plantPoints)
+    Width = getWidth(plantPoints)
+    Height = getHeight(plantPoints)
+    Compactness = getCompactness(Width, Height)
+    AveragePlantColour = getAveragePlantColour(plantPoints, picture)
+
+    displayProcesses(Score, picture, processedImage)
+
+    print(Score)
+    print(Width)
+    print(Height)
+    print("square " if Compactness == 0 else "rectangle")
+    print(AveragePlantColour)
+
     sys.stdout.flush()
