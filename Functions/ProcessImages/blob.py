d109ba51380c861dbaf27abaae5d925192917508
Karen McCulloch <k.a.z.mcc95@gmail.com>
2016-11-07 16:30:58 +0000

    start

--- Functions/ProcessImages/blob.py
+++ Functions/ProcessImages/blob.py
@@ -1,4 +1,19 @@
-blob = class():
+class blob():
 
     def __init__(self, coords):
+        self.points = coords
+        self.no_blobs = 1
+
+    def split():
+        pass
+
+    def pick():
+        pass
+
+class cardBlob(blob):
+    def __init__(self, coords):
+        blob.__init__(self, coords)
+
+    def split():
+        # make sure that all the coords in a blob are touching
 
