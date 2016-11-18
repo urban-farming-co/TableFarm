3fb3ae6d552e388479c33e3f0c4b1feef0d3e9ce
Karen McCulloch <k.a.z.mcc95@gmail.com>
2016-11-18 14:56:04 +0000

    hook up the submit button and activate a displayname

--- views/scripts/playfab.js
+++ views/scripts/playfab.js
@@ -6,34 +6,48 @@ var regAPI = url + "RegisterPlayFabUser";
 var updAPI = url + "UpdateUserData";
 
 $("#submitReg").click(function(){
-    console.log("tah");
-    alert("thanks paul");
     isValid = $("#email")[0].checkValidity() && 
         $("#type")[0].checkValidity() && 
-        $("#name")[0].checkValidity() && 
+        $("#username")[0].checkValidity() && 
         $("#password")[0].checkValidity();
-        $("#passwordConf")[0].checkValidity();
     if (!isValid){
-        return;
+        alert("Something is wrong! \nPlease check that you've entered the correct data.")
+            return;
     }
 
     var email = $("#email").val();
-    var name = $("#name").val();
+    var username = $("#username").val();
+    var displayname = $("#displayname").val();
     var type = $("#type").val();
     var password = $("#password").val();
     var deviceID = $("#deviceID").val();
     var plantspecies = $("#plantspecies").val();
     var plantname = $("#plantname").val();
 
-    Register(name, password,email, function(err, data){
+    Register(username,displayname, password,email, function(err, data){
+        if (err){
+            alert("something went wrong.\n" + JSON.stringify(err.error));
+        }
 
-        var user_session_ticket_value = 0; 
-        setAdditionalParameters(deviceID, plantspecies, plantname, user_session_ticket_value, function(err, data){
- //           window.location = "../userHome";
-        });
+        else{
+            var user_session_ticket_value = 0; 
+            try{
+                user_session_ticket_value = data.data.SessionTicket;
+            }catch(err){
+                user_session_ticket_value = JSON.parse(data.responseText).data.SessionTicket;
+            }
+            console.log(user_session_ticket_value);
+            setAdditionalParameters(deviceID, plantspecies, plantname, type, user_session_ticket_value, function(err, data){
+                if (err){
+                    alert("something went wrong.\n" + err);
+                }
+                else{
+                    window.location = "../userHome";
+                }
+
+            });
+        }
     });
-
-
 })
 
 $("#submitLog").click(function(){
@@ -43,45 +57,65 @@ $("#submitLog").click(function(){
 })
 
 
-function Register(name, password, email, callback){
+function Register(name,displayname, password, email, callback){
     console.log(regAPI);
     var headers = {"Content-Type": "application/json"};
     var data  = {
         "TitleId": titleID,
         "Username": name,
+        "DisplayName":displayname,
         "Email": email,
         "Password": password
     };
     console.log(JSON.stringify( data));
     $.ajax({
         type: "POST",
-            url: regAPI, 
-            data: JSON.stringify( data),
-            complete: function(data, status, hik){
-                callback(status, data)
-            },
-            error: function(err, dko){
-                console.error(err);
-            },
-            contentType: "application/json",
-            dataType:  "json"
+        url: regAPI, 
+        data: JSON.stringify( data),
+        complete: function(data, status){
+            console.log(data);
+            console.log("...");
+            console.log(JSON.parse(data.responseText))
+                callback(null, data);
+        },
+        error: function(err, dko){
+            console.error(err);
+            callback(JSON.parse(err.responseText));
+        },
+        contentType: "application/json",
+        dataType:  "json"
     });
 
 }
 
 
-function setAdditionalParameters(deviceID, plantspecies, plantname, user_session_ticket_value, callback){
+function setAdditionalParameters(deviceID, plantspecies, plantname, userType, user_session_ticket_value, callback){
     var headers = {"Content-Type": "application/json"};
     headers["X-Authentication"]= user_session_ticket_value ;
-    var data2 =  {
+    console.log(headers);
+    var data =  {
         "Data": {
             "deviceID": deviceID,
             "plantSpecies": plantspecies,
-            "plantName": plantname
+            "plantName": plantname,
+            "userType":userType
         },
         "Permission": "Public"
     }
-    callback();
+    $.ajax({
+        type: "POST",
+        url: updAPI, 
+        data: JSON.stringify( data),
+        complete: function(data, status){
+            callback(null, JSON.parse(data.responseText));
+        },
+        error: function(err, dko){
+            console.error(err);
+            callback(JSON.parse(err.responseText));
+        },
+        headers: headers,
+        dataType:  "json"
+    });
 }
 
 function Authenticate()
